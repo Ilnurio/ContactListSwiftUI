9191dcb32711104c6697ac76e0fd1c8a925c9ad4
Ilnur Fattakhov <nurich1995@mail.ru>
2023-07-06 18:16:34 +0300

    refactor DataModel and Person files

--- ContactListSwiftUI/Models/Person.swift
+++ ContactListSwiftUI/Models/Person.swift
@@ -5,32 +5,16 @@
 //  Created by Ilnur on 29.04.2023.
 //
 
-final class Person: Identifiable {
+struct Person: Identifiable {
     
-    let id: String
+    let id: Int
     let name: String
     let surname: String
-    let eMail: String
+    let email: String
     let mobilePhone: String
     
     var fullName: String {
         "\(name) \(surname)"
-    }
-    
-    var mobileInfo: String {
-        "Phone: \(mobilePhone)"
-    }
-    
-    var eMailInfo: String {
-        "Email: \(eMail)"
-    }
-    
-    init(id: String, name: String, surname: String, eMail: String, mobilePhone: String) {
-        self.id = id
-        self.name = name
-        self.surname = surname
-        self.eMail = eMail
-        self.mobilePhone = mobilePhone
     }
     
     static func getContactList() -> [Person] {
@@ -38,30 +22,20 @@ final class Person: Identifiable {
         var persons:[Person] = []
         // Данные берем из класса DataStore создавая экземпляры класса(массивы)
         // свойсто shuffled() перемешивает данные в массиве данных
-        let id = DataStore.shared.id.shuffled()
         let names = DataStore.shared.names.shuffled()
         let surnames = DataStore.shared.surnames.shuffled()
         let emails = DataStore.shared.emails.shuffled()
         let phones = DataStore.shared.mobilePhones.shuffled()
-        
-        // создаем свойство, которое принимает наименьшее количество элементов массива
-        let iterationCount = min(
-            id.count,
-            names.count,
-            surnames.count,
-            emails.count,
-            phones.count
-        )
-        
+
         // перебираем в цикле целые числа(индексы) для дальнейшего извлечения данных
         // внутри цикла инициализируем экземпляр модели
-        for index in 0..<iterationCount {
+        for index in 0..<names.count {
             let person = Person(
                 // передаем значение по индексу, таким образом берем первый элемент массива
-                id: id[index],
+                id: index + 1,
                 name: names[index],
                 surname: surnames[index],
-                eMail: emails[index],
+                email: emails[index],
                 mobilePhone: phones[index]
             )
             
@@ -70,15 +44,5 @@ final class Person: Identifiable {
         
         return persons
     }
-    
-    static func getOnePerson() -> Person {
-        let newPerson = Person(id: "1", name: "Ilnur", surname: "Fattakhov", eMail: "@mail,ru", mobilePhone: "888")
-        return newPerson
-    }
 }
 
-// чтобы не запоминать как называются системные кнопки можно сделать отдельное перечисление
-enum Contacts: String {
-    case phone = "phone"
-    case email = "tray"
-}
--- ContactListSwiftUI/Services/DataStore.swift
+++ ContactListSwiftUI/Services/DataStore.swift
@@ -9,20 +9,6 @@ final class DataStore {
     // обьявляем static свойство для синглтона
     static let shared = DataStore()
     
-    let id = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11"
-    ]
-    
     let names = [
         "Ilnur",
         "Julia",
